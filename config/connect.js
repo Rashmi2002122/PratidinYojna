79d2c0f37c5a730c961744f3ad9dd79002c36f66
Rashmi ranjan jena <143921669+Rashmi2002122@users.noreply.github.com>
2024-11-18 16:27:33 +0530

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const conn=mongoose
-  .connect("mongodb://localhost:27017/project")
+  .connect("mongodb://127.0.0.1:27017/project")
   .then(function () {
     console.log("connected");
   })
